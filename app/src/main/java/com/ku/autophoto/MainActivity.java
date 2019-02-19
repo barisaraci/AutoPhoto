ef49a834570ffe94101a8eede726a7970fecb04e
baris <dbarisaraci@gmail.com>
2019-02-19 22:15:08 +0300

    - Add Camera Activity - Edit colors.xml, styles.xml, AndroidManifest.xml, build.gradle

--- app/src/main/java/com/ku/autophoto/MainActivity.java
+++ app/src/main/java/com/ku/autophoto/MainActivity.java
@@ -1,13 +1,37 @@
 package com.ku.autophoto;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        context = this;
+        initLayout();
+    }
+
+    private void initLayout() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, CameraActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
