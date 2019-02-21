eae1e6bd15fa4d3aa53a9cc97f550b5233e0b382
baris <dbarisaraci@gmail.com>
2019-02-21 22:16:45 +0300

    - Migrate to AndroidX

--- app/src/main/java/com/ku/autophoto/CameraActivity.java
+++ app/src/main/java/com/ku/autophoto/CameraActivity.java
@@ -1,9 +1,7 @@
 package com.ku.autophoto;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class CameraActivity extends AppCompatActivity {
 
--- app/src/main/java/com/ku/autophoto/MainActivity.java
+++ app/src/main/java/com/ku/autophoto/MainActivity.java
@@ -1,12 +1,10 @@
 package com.ku.autophoto;
 
 import android.content.Context;
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.widget.Button;
+import androidx.appcompat.widget.Toolbar;
 
 import com.camerakit.CameraKitView;
 
