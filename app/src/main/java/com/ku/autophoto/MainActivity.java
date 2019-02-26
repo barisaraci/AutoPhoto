e16ce9c4baa20621ad115b56fd2559366780ade1
baris <dbarisaraci@gmail.com>
2019-02-26 20:18:43 +0300

    - Fix

--- app/src/main/java/com/ku/autophoto/MainActivity.java
+++ app/src/main/java/com/ku/autophoto/MainActivity.java
@@ -229,7 +229,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        camera.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        //camera.onRequestPermissionsResult(requestCode, permissions, grantResults);
         switch (requestCode) {
             case MY_PERMISSIONS_REQUEST_SNAP: {
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
